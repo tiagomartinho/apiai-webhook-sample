b9426025904e633ed2e01bae1ece1aa0d41a99b9
Tiago Martinho <tiago@elit.software>
2017-05-07 01:48:58 +0200

    added video stream

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-var path = require("path");
+var fs = require("fs"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path");
 const restService = express();
 restService.use(bodyParser.json());
 
@@ -35,6 +38,43 @@ restService.get('/state', function (req, res) {
 
 restService.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+restService.get('/video.mp4', function(req, res) {
+  var file = path.resolve(__dirname,"video.mp4");
+  fs.stat(file, function(err, stats) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // 404 Error if file not found
+        return res.sendStatus(404);
+      }
+    res.end(err);
+    }
+    var range = req.headers.range;
+    if (!range) {
+     // 416 Wrong range
+     return res.sendStatus(416);
+    }
+    var positions = range.replace(/bytes=/, "").split("-");
+    var start = parseInt(positions[0], 10);
+    var total = stats.size;
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    });
+
+    var stream = fs.createReadStream(file, { start: start, end: end })
+      .on("open", function() {
+        stream.pipe(res);
+      }).on("error", function(err) {
+        res.end(err);
+      });
+  });
 });
 
 restService.listen((process.env.PORT || 5000), function () {
